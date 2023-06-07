fea0d0d3b4fc78d768adc1889c3641ddf3f174c1
Melissa Swearingen <swearingenmj@gmail.com>
2023-06-07 14:04:47 -0700

    add use for users, courses, and assignmnets

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+
+const router = module.exports = require('express').Router();
+
+router.use('/users', require('./users').router);
+router.use('/courses', require('./courses').router);
+router.use('/assignments', require('./assignments').router);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
