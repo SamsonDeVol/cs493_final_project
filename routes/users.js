b5e5e4230c5a661fa29c0f1a6349e216c5dea464
Melissa Swearingen <swearingenmj@gmail.com>
2023-06-07 14:05:17 -0700

    add comments and pseudocode

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+exports.router = router;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('accessing the /users route');
 });
+
+/* POST - create a new user */
+
+
+/* POST - login a user */
+
+
+/* GET - get a user by id */ 
 
 module.exports = router;
