0af209eac016ffb0c687bad9be40d7920425755c
SamsonDeVol <devols@oregonstate.edu>
2023-06-08 21:14:43 -0700

    update users route, initialization, and data

--- initDb.js
+++ initDb.js
@@ -6,9 +6,12 @@
 const sequelize = require('./lib/sequelize')
 const { Course, CourseClientFields } = require('./models/course')
 const { Submission, SubmissionClientFields } = require('./models/submissions')
+const { User, UserClientFields } = require('./models/user')
 
 const courseData = require('./data/courses.json')
+const userData = require('./data/users.json')
 
 sequelize.sync().then(async function () {
   await Course.bulkCreate(courseData, { fields: CourseClientFields })
+  await User.bulkCreate(userData, { fields: UserClientFields })
 })
--- routes/users.js
+++ routes/users.js
@@ -7,12 +7,45 @@ router.get('/', function(req, res, next) {
   res.send('accessing the /users route');
 });
 
+
+const { ValidationError } = require('sequelize');
+const { validateAgainstSchema } = require('../lib/validation');
+const { Course, CourseClientFields } = require('../models/course');
+const { User, UserClientFields } = require('../models/user');
+
+/* 
+ * Route to return a list of all Courses.
+ * This list should be paginated. The Courses returned 
+ * should not contain the list of students in the Course or the 
+ * list of Assignments for the Course.
+ */ 
+
+
 /* POST - create a new user */
+
+router.post('/', async function (req, res) {
+  try {
+    const user = await User.create(req.body, UserClientFields)
+    res.status(201).send({ id: user.id })
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      res.status(400).send({ error: e.message })
+    } else {
+      throw e
+    }
+  }
+});
 
 
 /* POST - login a user */
-
+router.post('/login', async function(req, res) {
+  res.status(200).send('welcome to login')
+})
 
 /* GET - get a user by id */ 
 
+router.get('/:id', async function(req, res) {
+  console.log("id", req.params.id)
+  res.status(200).send(`looking for user with id ${req.params.id}`)
+});
 module.exports = router;
