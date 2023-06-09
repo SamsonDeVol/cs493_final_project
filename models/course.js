9cb0d549664f50562749fe508f399d2a75c0f720
SamsonDeVol <devols@oregonstate.edu>
2023-06-08 21:39:39 -0700

    add autoIncrement to id for user and course models

--- models/course.js
+++ models/course.js
@@ -4,14 +4,14 @@ const sequelize = require('../lib/sequelize')
 const { User } = require('./user')
 
 const Course = sequelize.define('course', {
-  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
   subject_code: { type: DataTypes.STRING, allowNull: false },
   number: { type: DataTypes.STRING, allowNull: false },
   title: { type: DataTypes.STRING, allowNull: false },
   term: { type: DataTypes.STRING, allowNull: false },
   instructor_id: { type: DataTypes.INTEGER, allowNull: false }
 })
-
+exports.Course = Course
 /*
 * Set up one-to-many relationship between Course and User.
 */
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize')
 const sequelize = require('../lib/sequelize')
 
 const User = sequelize.define('user', {
-    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.TEXT, allowNull: false },
