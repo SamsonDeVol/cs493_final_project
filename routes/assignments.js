f81f0153286b0cb787de8b049a018a7534942228
Ian Snyder <isnide23@gmail.com>
2023-06-09 10:43:14 -0700

    stub out submissions post and get endpoints references #6

--- routes/assignments.js
+++ routes/assignments.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const { Submission, SubmissionClientFields } = require('../models/submission')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('accessing the /assignments route');
@@ -8,115 +10,67 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
+/*
+ * Route to create a new submission.
+ */
+router.post('/{id}/submissions', async function (req, res, next) {
+  try {
+    const submission = await submission.create(req.body, SubmissionClientFields)
+    res.status(201).send({ id: submission.id })
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      res.status(400).send({ error: e.message })
+    } else {
+      throw e
+    }
+  }
+})
+
+
+/*
+ * Route to return a list of submissions.
+ */
+router.get('/{id}/submissions', async function (req, res) {
+  /*
+   * Compute page number based on optional query string parameter `page`.
+   * Make sure page is within allowed bounds.
+   */
+  let page = parseInt(req.query.page) || 1
+  page = page < 1 ? 1 : page
+  const numPerPage = 10
+  const offset = (page - 1) * numPerPage
+
+  const result = await Submission.findAndCountAll({
+    limit: numPerPage,
+    offset: offset
+  })
+
+  /*
+   * Generate HATEOAS links for surrounding pages.
+   */
+  const lastPage = Math.ceil(result.count / numPerPage)
+  const links = {}
+  if (page < lastPage) {
+    links.nextPage = `/submissions?page=${page + 1}`
+    links.lastPage = `/submissions?page=${lastPage}`
+  }
+  if (page > 1) {
+    links.prevPage = `/submissions?page=${page - 1}`
+    links.firstPage = '/submissions?page=1'
+  }
+
+  /*
+   * Construct and send response.
+   */
+  res.status(200).json({
+    submissions: result.rows,
+    pageNumber: page,
+    totalPages: lastPage,
+    pageSize: numPerPage,
+    totalCount: result.count,
+    links: links
+  })
+})
 
 
 
-// const router = require('express').Router()
-// const { validateAgainstSchema } = require('../lib/validation')
-// const { businessSchema, createBusinessesTable, getBusinessPage, getBusinessById, insertNewBusiness, updateBusinessById, deleteBusinessById } = require('../models/business')
-// const businesses = require('../data/businesses')
-
-// exports.router = router
-// exports.businesses = businesses
-
-// // Route to create business table. 
-// router.post('/createBusinessesTable', async (req, res) => {
-//   try {
-//     await createBusinessesTable()
-//     res.status(200).send({})
-//   } catch (err) {
-//     res.status(500).json({
-//       error: "Error creating businesses table (may already exist)"
-//     })
-//   }
-// })
-
-// // Route to return a list of businesses.
-// router.get('/', async (req, res) => {
-// try {
-//   const businessesPage = await getBusinessPage(parseInt(req.query.page) || 1)
-//   res.status(200).send(businessesPage)
-// } catch (err) {
-//   res.status(500).json({
-//     error: "Error fetching businesses list"
-//   })
-// }
-// })
-
-// // Route to create a new business.
-// router.post('/', async (req, res) => {
-//   if (validateAgainstSchema(req.body, businessSchema)) {
-//     try {
-//       const id = await insertNewBusiness(req.body)
-//       res.status(201).json({
-//         id: id,
-//         links: {
-//           business: `/businesses/${id}`
-//         }
-//       })
-//     } catch (err) {
-//       console.log("err", err)
-//       res.status(500).json({
-//         error: `Error inserting business into database ${err}`
-//       })
-//     }
-//   } else {
-//     res.status(400).json({
-//       error: "Request body is not a valid business object"
-//     })
-//   }
-// })
-
-// // Route to fetch info about a specific business. 
-// router.get('/:businessid', async (req, res, next) => {
-//   try {
-//     const business = await getBusinessById(req.params.businessid)
-//     res.status(200).json(business)
-//   } catch {
-//     next()
-//   }
-// })
-
-// // Route to replace data for a business.
-// router.put('/:businessid', async function (req, res, next) {
-//   if (validateAgainstSchema(req.body, businessSchema)) {
-//     try {
-//       const updateSucessful = await updateBusinessById(req.params.businessid, req.body)
-//       if (updateSucessful) {
-//         res.status(200).send({
-//           id: req.params.businessid,
-//           links: {
-//             business: `/businesses/${req.params.businessid}`
-//           }})
-//       } else {
-//         next()
-//       }
-//     } catch (err) {
-//       res.status(500).json({
-//         error: "Unable to update business"
-//       })
-//     }
-//   } else {
-//     res.status(400).json({
-//       error: "Request body is not a valid business object"
-//     })
-//   }
-  
-// })
-
-// // Route to delete a business.
-// router.delete('/:businessid', async function (req, res, next) {
-//   try {
-//     const deleteSuccessful = await deleteBusinessById(req.params.businessid)
-
-//     if (deleteSuccessful) {
-//             res.status(204).end()
-//     } else {
-//         next()
-//     }
-// } catch (err) {
-//     res.status(500).send({
-//         error: "Unable to delete business."
-//     })
-// }
-// })
