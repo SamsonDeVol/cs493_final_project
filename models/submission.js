5f3eb5b868ceb43569a97ae23de62d1f4c1905b7
SamsonDeVol <devols@oregonstate.edu>
2023-06-14 14:24:34 -0700

    uploading and downloading file for submissions

--- models/submission.js
+++ models/submission.js
@@ -6,9 +6,8 @@ const Submission = sequelize.define('submissions', {
     id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
     assignmentId: {type: DataTypes.INTEGER, allowNull: false},
     studentId: {type: DataTypes.INTEGER, allowNull: false},
-    // timestamp: {type: DataTypes.DATE, allowNull: false}, // sequelize should auto include createdAt 
     grade: { type: DataTypes.FLOAT, allowNull: true },
-    file: { type: DataTypes.BLOB, length: "medium", allowNull: false },
+    filePath: { type: DataTypes.STRING, allowNull: false },
     fileType: { type: DataTypes.STRING, allowNull: false }
 })
 exports.Submission = Submission
@@ -16,62 +15,15 @@ exports.Submission = Submission
 exports.SubmissionClientFields = [
     'assignmentId',
     'studentId',
-    // 'timestamp',
     'grade',
-    'file',
+    'filePath',
     'fileType'
 ]
 
-// async function getSubmissionsCount() {
-//     const [ count ] = await mysqlPool.query(
-//         "SELECT COUNT(*) AS count FROM submissions"
-//     )
-//     return count
-// }
-
-// async function getSubmissionsPage(page) {
-//     const count = await getSubmissionsCount()
-//     const [ submissionPage ] = await mysqlPool.query(
-//         "SELECT * FROM submissions"
-//     )
-//     const numPerPage = 1
-//     const lastPage = Math.ceil(submissionPage.length / numPerPage)
-//     page = page > lastPage ? lastPage : page
-//     page = page < 1 ? 1 : page
-
-//     const start = (page - 1) * numPerPage
-//     const end = start + numPerPage
-//     const pageSubmissions = submissionPage.slice(start, end)
-
-//     const links = {}
-//     if (page < lastPage) {
-//         links.nextPage = `/submissions?page=${page + 1}`
-//         links.lastPage = `/submissions?page=${lastPage}`
-//     }
-//     if (page > 1) {
-//         links.prevPage = `/submissions?page=${page - 1}`
-//         links.firstPage = '/submissions?page=1'
-//     }
-
-//     return {
-//         submissions: pageSubmissions,
-//         pageNumber: page,
-//         totalPages: lastPage,
-//         pageSize: numPerPage,
-//         totalCount: count,
-//         links: links
-//     }
-// }
-
-// async function getSubmissonByStudentId(studentId) {
-//     const query = `SELECT * FROM submissions WHERE studentId=${studentId}`
-//     const [ submission ] = await mysqlPool.query(query)
-//     return submission
-// }
-
-
-// exports.submissionSchema = submissionSchema
-// exports.createSubmissionsTable = createSubmissionsTable
-// exports.getSubmissionCount = getSubmissionsCount
-// exports.getSubmissionPage = getSubmissionsPage
-// exports.getsubmissionByStudentId = getsubmissionByStudentId
+exports.fileTypes = {
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "text/csv": "csv",
+    "application/pdf": "pdf",
+    "application/zip": "zip"
+}
--- routes/assignments.js
+++ routes/assignments.js
@@ -1,10 +1,30 @@
 var express = require('express');
 var router = express.Router();
+
+const crypto = require("node:crypto")
+var fs = require('fs');
+
+const { requireAuthentication } = require('../lib/auth')
+const { Submission, SubmissionClientFields, fileTypes } = require('../models/submission')
+const { Course } = require('../models/course')
+const { User } = require('../models/user')
 const multer = require('multer');
-const upload = multer({storage: multer.memoryStorage()});
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: `${__dirname}/../uploads`,
+    filename: (req, file, callback) => {
+        const filename = crypto.pseudoRandomBytes(16).toString("hex")
+        const extension = fileTypes[file.mimetype]
+        callback(null, `${filename}.${extension}`)
+    }
+  }),
+  fileFilter: (req, file, callback) => {
+    callback(null, !!fileTypes[file.mimetype]);
+  }
+});
 const { ValidationError } = require('sequelize')
 const { Assignment, AssignmentClientFields } = require('../models/assignment')
-const { Submission, SubmissionClientFields } = require('../models/submission')
+
 
 module.exports = router;
 
@@ -81,64 +101,110 @@ router.delete('/:id', async function (req, res) {
   
 })
 
+router.get('/submissions/:id/download', requireAuthentication, async function (req, res) {
+  // try to get file from submission file upload
+  try{
+    const submission = await Submission.findByPk(req.params.id)
+    if(!submission){
+      res.status(400).send("cannot find submission with that id")
+    }
+    var file = submission.filePath;
+    var filename = file.split('.');
+    var mimetype = submission.fileType;
+  
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+  
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+  } catch {
+    res.status(400).send("error downloading file.")
+  }  
+})
 /*
  * Route to return a list of submissions for an assignment.
  */
-router.get('/:id/submissions', async function (req, res) {
+router.get('/:id/submissions', requireAuthentication, async function (req, res) {
   /*
    * Compute page number based on optional query string parameter `page`.
    * Make sure page is within allowed bounds.
    */
-  console.log(req.params.id)
   const id = req.params.id
-  let page = parseInt(req.query.page) || 1
-  page = page < 1 ? 1 : page
-  const numPerPage = 10
-  const offset = (page - 1) * numPerPage
+  // try to get assignment data
+  try{ 
+    const assignment = await Assignment.findByPk(id)
+    const course = await Course.findByPk(assignment.courseId)
+    const instructor = await User.findByPk(course.instructorId)
 
-  const result = await Submission.findAndCountAll({
-    limit: numPerPage,
-    offset: offset
-  })
+    // verify user matches requirements for query
+    if (req.role == 'admin' || req.user == instructor.id){
+      let page = parseInt(req.query.page) || 1
+      page = page < 1 ? 1 : page
+      const numPerPage = 10
+      const offset = (page - 1) * numPerPage
+    
+      const result = await Submission.findAndCountAll({
+        limit: numPerPage,
+        offset: offset
+      })
 
-  /*
-   * Generate HATEOAS links for surrounding pages.
-   */
-  const lastPage = Math.ceil(result.count / numPerPage)
-  const links = {}
-  if (page < lastPage) {
-    links.lastPage = `/${id}/submissions?page=${lastPage}`
-    links.nextPage = `/${id}/submissions?page=${page + 1}`
+      console.log("res: ", result)
+    
+      /*
+      * Generate HATEOAS links for surrounding pages.
+      */
+      const lastPage = Math.ceil(result.count / numPerPage)
+      const links = {}
+      if (page < lastPage) {
+        links.lastPage = `/assignments/${id}/submissions?page=${lastPage}`
+        links.nextPage = `/assignments/${id}/submissions?page=${page + 1}`
+      } else if (page > 1) {
+        links.prevPage = `/assignments/${id}/submissions?page=${page - 1}`
+        links.firstPage = `/assignments/${id}/submissions?page=1`
+      } else {
+        links.firstPage = `/assignments/${id}/submissions?page=1`
+      }
+    
+      /*
+      * Construct and send response.
+      */
+      for (const subIndex in result.rows) {
+        // result.row[subIndex].downloadLink = '/assignment'
+        console.log("submission id", result.rows[subIndex]);
+        result.rows[subIndex].dataValues.downloadLink = `/assignments/submissions/${result.rows[subIndex].id}/download`
+
+      }
+      
+      console.log("result", result.rows)
+
+      res.status(200).json({
+        submissions: result.rows,
+        pageNumber: page,
+        totalPages: lastPage,
+        pageSize: numPerPage,
+        totalCount: result.count,
+        links: links
+      })
+    } else {
+      res.status(403).send("The request was not made by an authenticated User")
+    } 
+  } catch {
+    res.status(404).send(`Specified Assignment with id: ${id} not found`)
   }
-  if (page > 1) {
-    links.prevPage = `/${id}/submissions?page=${page - 1}`
-    links.firstPage = `/${id}/submissions?page=1`
-  }
-
-  /*
-   * Construct and send response.
-   */
-  res.status(200).json({
-    submissions: result.rows,
-    pageNumber: page,
-    totalPages: lastPage,
-    pageSize: numPerPage,
-    totalCount: result.count,
-    links: links
-  })
 })
 
 /*
  * Route to create a new submission.
  */
-router.post('/:id/submissions', upload.single('file'), async function (req, res, next) {
-  console.log("req stuff", req)
+router.post('/:id/submissions', requireAuthentication, upload.single('file'), async function (req, res, next) {
+  console.log("req stuff", req.file.mimetype)
+  console.log(" req file path", req.file.path)
   try {
-    const submission = await submission.create({
+    const submission = await Submission.create({
       'assignmentId': req.params.id,
-      'studentId': 0, //get from authentication
+      'studentId': 7, //get from authentication
       'grade': -1,
-      'file': req.file.buffer,
+      'filePath': req.file.path,
       'fileType': req.file.mimetype
     }, SubmissionClientFields)
     res.status(201).send({ id: submission.id })
